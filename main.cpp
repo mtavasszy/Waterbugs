c8e734fee598487ecbe882b285908bfd4dd34eed
mtavasszy <matt.tavasszy@gmail.com>
2021-12-02 23:49:24 +0100

    fps limit with standard SFML instead of custom code

--- main.cpp
+++ main.cpp
@@ -1,24 +1,20 @@
-#include <chrono>
-#include <thread>
 #include <SFML/Graphics.hpp>
 #include "simulation.h"
 
 int main()
 {
-    float targetFPS = 60;
-    int targetFrameTimeMs = 1000 / targetFPS;
+    int targetFPS = 60;
 
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(sf::VideoMode(1280, 720), "Waterbugs", sf::Style::Default, settings);
+    window.setFramerateLimit(targetFPS);
     sf::Color clearColor(14, 135, 204, 255);
 
     Simulation simulation = Simulation();
 
     while (window.isOpen())
     {
-        auto frameStart = std::chrono::steady_clock::now();
-
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -30,15 +26,6 @@ int main()
         window.clear(clearColor);
         simulation.draw(window);
         window.display();
-
-        auto frameEnd = std::chrono::steady_clock::now();
-        auto frameTime = frameEnd - frameStart;
-        while (frameTime < std::chrono::milliseconds(targetFrameTimeMs))
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-            frameEnd = std::chrono::steady_clock::now();
-            frameTime = frameEnd - frameStart;
-        }
     }
 
     return 0;
