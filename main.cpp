13c19cba221daa37434cce9d2de1fc11096dafc7
mtavasszy <matt.tavasszy@gmail.com>
2021-12-03 16:00:18 +0100

    fps and creature count in window title

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <SFML/Graphics.hpp>
+#include <sstream>
 #include "simulation.h"
 
 int main()
 {
     int targetFPS = 60;
+    float fps = targetFPS;
+    float frameCount = 0;
     sf::Vector2f windowSize(1280, 720);
 
     sf::ContextSettings settings;
@@ -13,6 +16,8 @@ int main()
     sf::Color clearColor(14, 135, 204, 255);
 
     Simulation simulation(windowSize);
+
+    sf::Clock clock;
 
     while (window.isOpen())
     {
@@ -27,6 +32,21 @@ int main()
         window.clear(clearColor);
         simulation.draw(window);
         window.display();
+        
+        // Update title info
+        std::string creatureCountStr = std::to_string(simulation.creatures.size()) + " creature" + (simulation.creatures.size() > 1 ? "s" : "");
+
+        frameCount++;
+        if (clock.getElapsedTime().asSeconds() >= 1.f) {
+            fps = frameCount / clock.restart().asSeconds();
+            frameCount = 0;
+        }
+        std::ostringstream out;
+        out.precision(2);
+        out << std::fixed << fps;
+        std::string fpsStr = "fps: " + out.str();
+
+        window.setTitle("Waterbugs, " + creatureCountStr + ", " + fpsStr);
     }
 
     return 0;
